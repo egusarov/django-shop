70d2529141919ae3ef59079730ef95edad169b89
Evgeniy Gusarov <gusarov08@gmail.com>
2025-11-07 17:25:10 +0200

    Models registered in adminsite

--- src/apps/products/admin.py
+++ src/apps/products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "parent", "created_at")
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ("name",)
+    list_filter = ("parent",)
+    ordering = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price", "stock", "created_at")
+    prepopulated_fields = {"slug": ("name",)}
+    list_filter = ("category",)
+    search_fields = ("name", "description")
+    ordering = ("name",)
--- src/apps/reviews/admin.py
+++ src/apps/reviews/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("product", "user", "rating", "created_at")
+    list_filter = ("rating", "created_at")
+    search_fields = ("comment", "user__username", "product__name")
+    ordering = ("-created_at",)
