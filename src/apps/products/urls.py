d5418fa07ba46dbb6bb0ec81d4d469e39a11d05e
Evgeniy Gusarov <gusarov08@gmail.com>
2025-11-11 12:14:43 +0200

    feat(templates): add product_detail.html

--- src/apps/products/urls.py
+++ src/apps/products/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<int:pk>/', views.product_detail, name='product_detail'),
+    path("<slug:slug>/", views.product_detail, name="product_detail"),
 ]
--- src/apps/products/views.py
+++ src/apps/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from ..reviews.models import Review
 
 
 def home(request):
@@ -7,6 +8,10 @@ def home(request):
     return render(request, 'pages/home.html', {'products': products})
 
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, 'pages/product_detail.html', {'product': product})
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    reviews = Review.objects.filter(product=product).order_by('-created_at')[:3]
+    return render(request, 'pages/product_detail.html', {
+        'product': product,
+        'reviews': reviews
+    })
